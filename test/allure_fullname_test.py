1d9ae8d9429b68b5bf2ec31785e848a750317bd2
OlgaChatalyan <106739669+OlgaChatalyan@users.noreply.github.com>
2025-07-16 16:52:44 +0300

    Update allure_fullname_test.py

--- test/allure_fullname_test.py
+++ test/allure_fullname_test.py
@@ -2,7 +2,7 @@ import pytest
 from allure_commons.reporter import AllureReporter
 from allure_pytest.listener import AllureListener
 
-
+@allure.severity("Major")
 @pytest.fixture()
 def allure_reporter(request) -> AllureReporter:
     listener: AllureListener = next(
