bd4ec0d0038b4893a085c4ddb239a1ad78b9bc8d
OlgaChatalyan <106739669+OlgaChatalyan@users.noreply.github.com>
2025-09-18 10:47:12 +0300

    Update allure_status_test.py

--- test/allure_status_test.py
+++ test/allure_status_test.py
@@ -4,32 +4,32 @@ from allure import attachment_type
 from allure_commons.types import Severity
 
 @allure.epic("Allure TestOps")
-@allure.feature("Google search")
-@allure.story("The cutest animal")
-
 def test_passed():
     pass
 
-
+@allure.epic("Allure TestOps")
 def test_failed():
     assert False, "Some fail reason"
 
-
+@allure.epic("Allure TestOps")
 def test_broken():
     var = 1 / 0
     assert var
 
 
 @pytest.mark.skip(reason="Some skip reason")
+@allure.epic("Allure TestOps")
 def test_skipped():
     pass
 
 
 @pytest.mark.xfail(reason="Some xfail reason")
+@allure.epic("Allure TestOps")
 def test_xfail():
     assert False, "Assertion failed"
 
 
 @pytest.mark.xfail(reason="Test doesn't fail")
+@allure.epic("Allure TestOps")
 def test_xpass():
     pass
