c865e2b23b0b43594e5a28b694822245e5a94298
OlgaChatalyan <106739669+OlgaChatalyan@users.noreply.github.com>
2025-09-18 11:09:34 +0300

    Update allure_title.py

--- test/allure_title.py
+++ test/allure_title.py
@@ -3,17 +3,16 @@ import pytest
 from allure import attachment_type
 from allure_commons.types import Severity
 
-@allure.epic("Allure TestOps")
-@allure.feature("Attachments")
-@allure.story("Add atachment")
 
 @allure.title("Some title for test")
+@allure.epic("Allure TestOps")
 def test_with_title():
     pass
 
 
 @allure.title("Test title with param {param}")
 @pytest.mark.parametrize("param", ["first", "second"])
+@allure.epic("Allure TestOps")
 def test_with_dynamic_title(param):
     pass
 
@@ -23,6 +22,6 @@ def test_with_dynamic_title(param):
 def titled_fixture():
     pass
 
-
+@allure.epic("Allure TestOps")
 def test_with_titled_fixture(titled_fixture):
     pass
