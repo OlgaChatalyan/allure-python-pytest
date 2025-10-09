4a8531d7077ddff8afff39ac4b98bc37e2f19bfe
OlgaChatalyan <106739669+OlgaChatalyan@users.noreply.github.com>
2025-10-09 17:50:29 +0300

    Create tests_as_code.py

---
+++ test/tests_as_code.py
@@ -0,0 +1,13 @@
+import allure
+
+@allure.title("Демонстрационный ручной тест")
+@allure.story("Story")
+@allure.feature("Feature")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("example")
+@allure.label("owner", "mlankin")
+@allure.manual(True)
+def test_demo_manual():
+    with allure.step("Демонстрационный шаг"):
+        pass
+    custom_step()
