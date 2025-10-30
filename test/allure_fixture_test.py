b40392a9daca8e1357c813fe2b63cd3718df1b2f
OlgaChatalyan <106739669+OlgaChatalyan@users.noreply.github.com>
2025-10-31 00:35:46 +0300

    Update allure_fixture_test.py

--- test/allure_fixture_test.py
+++ test/allure_fixture_test.py
@@ -8,7 +8,7 @@ def titled_fixture():
     pass
 
 @allure.epic("Allure TestOps")
-@allure.label("tag", "For_testplan")
+@allure.label("tag", "Regression")
 @allure.title("Fixture title")
 def test_with_fixture_title(titled_fixture):
     pass
@@ -83,13 +83,13 @@ def function_level_fixture(request):
     request.addfinalizer(finalizer)
 
 @allure.epic("Allure TestOps")
-@allure.label("tag", "For_testplan")
+@allure.label("tag", "Regression")
 def test_allure_yield_fixture(session_level_yield_fixture, module_level_yield_fixture, function_level_yield_fixture):
     with allure.step("Step inside test_allure_yield_fixture"):
         pass
 
 @allure.epic("Allure TestOps")
-@allure.label("tag", "For_testplan")
+@allure.label("tag", "Regression")
 def test_allure_fixture_with_finalizer(session_level_fixture, module_level_fixture, function_level_fixture):
     with allure.step("Step inside test_allure_fixture_with_finalizer"):
         pass
